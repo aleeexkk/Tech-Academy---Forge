86c47442ccebf12a07fe1e9c8931961677622e04
ALEX <alexvdasilva3@gmail.com>
2026-03-16 22:40:35 -0300

    aula sobre js

---
+++ index.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+let nome = prompt('Digite seu nome: ');
+console.log("O nome digitado foi: " + nome);
+
+let cor = prompt('Digite sua cor favorita: ');
+console.log("A cor favorita digitada foi: " + cor);
+
+let filme = prompt('Digite seu filme favorito: ');
+console.log("O filme favorito digitado foi: " + filme);
+
+let cidade = prompt('Digite sua cidade natal: ');
+console.log("A cidade natal digitada foi: " + cidade);
+
+let animal = prompt('Digite seu animal de estimação favorito: ');
+console.log("O animal de estimação favorito digitado foi: " + animal);
