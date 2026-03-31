be7755c9248e88d071db3403bc045b41a2818e5a
ALEX <alexvdasilva3@gmail.com>
2026-03-30 21:51:53 -0300

    let a,b,c,d,e, maiorValor;

---
+++ aula2.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+
+let a,b,c,d,e, maiorValor;
+
+a = Number(prompt("Informe o Valor: "));
+b = Number(prompt("Informe o Valor: "));
+c = Number(prompt("Informe o Valor: "));
+d = Number(prompt("Informe o Valor: "));
+e = Number(prompt("Informe o Valor: "));
